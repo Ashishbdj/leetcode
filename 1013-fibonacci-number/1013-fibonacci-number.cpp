01ef0037924fcd6ca100e2d7b4cc65076cbb4db1
Ashish Bhardwaj <124652105+Ashishbdj@users.noreply.github.com>
2025-09-09 09:58:00 +0530

    Time: 0 ms (100%), Space: 7.7 MB (87.15%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0) return 0;
+        int p=1,pp=0,ans=0;
         if(n==1) return 1;
-
-        return fib(n-1)+fib(n-2);
+        for(int i=2;i<=n;i++)
+        {
+            ans=p+pp;
+            pp=p;
+            p=ans;
+        }
+        return ans;
     }
 };
