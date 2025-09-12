61fc6a542329a2a9d20d09d4774f1e67febbcf3a
Ashish Bhardwaj <124652105+Ashishbdj@users.noreply.github.com>
2025-09-12 20:03:04 +0530

    Time: 0 ms (100%), Space: 10.3 MB (96.7%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
 
-    void printsubset(vector<int>& nums,vector<int>& s,int i,set<vector<int>>& ans)
+    void printsubset(vector<int>& nums,vector<int>& s,int i,vector<vector<int>>& ans)
     {
         if(i==nums.size())
         {
-            ans.insert(s);
+            ans.push_back(s);
             return;
         }
         s.push_back(nums[i]);
         printsubset(nums,s,i+1,ans);
         s.pop_back();
-        printsubset(nums,s,i+1,ans);
+        int index=i+1;
+        while(index<nums.size()&&nums[i]==nums[index])index++;
+        printsubset(nums,s,index,ans);
 
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<int> se;
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
         printsubset(nums,se,0,ans);
-        return vector<vector<int>>(ans.begin(),ans.end());
+        return ans;
     }
 };
