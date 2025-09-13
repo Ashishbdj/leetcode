2932bf268b6779fcdc42ba76615c1970a55e00e6
Ashish Bhardwaj <124652105+Ashishbdj@users.noreply.github.com>
2025-09-13 18:03:21 +0530

    Time: 3 ms (48.61%), Space: 9.2 MB (21.51%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,56 @@
+class Solution {
+    public:
+    void queen(vector<string> &board,int r,int n,vector<vector<string>> &ans)
+    {
+        if(r==n)
+        {
+            ans.push_back({board});
+            return;
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            if(findPosition(board,r,i,n))
+            {
+                board[r][i]='Q';
+                queen(board,r+1,n,ans);
+                board[r][i]='.';
+            }
+        }
+    }
+    bool findPosition(vector<string>&board,int r,int c,int n)
+    {
+        //horizontal
+        for(int i=0;i<n;i++)
+        {
+            if(board[r][i]=='Q')
+                return false;
+        }
+    //vertical
+        for(int j=0;j<n;j++)
+        {
+            if(board[j][c]=='Q')
+                return false;
+        }
+    //Diagonal
+        for(int i=r,j=c;i>=0&&j>=0;i--,j--)
+        {
+            if(board[i][j]=='Q')
+                return false;
+        }
+
+        for(int i=r,j=c;i>=0&&j<n;i--,j++)
+        {
+            if(board[i][j]=='Q')
+                return false;
+        }
+
+        return true;
+    }
+    int totalNQueens(int n) {
+        vector<vector<string>>ans;
+        vector<string>board(n,string(n,'.'));
+        queen(board,0,n,ans);
+        return ans.size();
+    }
+};
